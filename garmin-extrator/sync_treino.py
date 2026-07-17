3adfd608bf1bc39b445bb6076cd61575e87e7aca
Bruno Biazon <brunobiazon10@gmail.com>
2026-07-17 10:32:38 -0300

    Add files via upload

---
+++ garmin-extrator/sync_treino.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from garminconnect import Garmin
+
+email = os.environ.get("GARMIN_EMAIL")
+senha = os.environ.get("GARMIN_PASSWORD")
+webhook_n8n = os.environ.get("N8N_WEBHOOK")
+
+try:
+    client = Garmin(email, senha)
+    client.login()
+    treinos = client.get_activities(0, 5) 
+
+    if treinos:
+        payload = []
+        for treino in treinos:
+            duracao_segundos = treino.get("duration")
+            
+            payload.append({
+                "id_treino": str(treino.get("activityId")), 
+                "nome_treino": treino.get("activityName"),
+                "distancia_metros": treino.get("distance"),
+                "tempo_minutos": round(duracao_segundos / 60, 2) if duracao_segundos else None,
+                "calorias": treino.get("calories"),
+                "data_inicio": treino.get("startTimeLocal"),
+                "bpm_medio": treino.get("averageHR"),
+                "bpm_maximo": treino.get("maxHR") 
+            })
+            
+        resposta = requests.post(webhook_n8n, json={"treinos": payload})
+        print(f"Webhook acionado. Status: {resposta.status_code}")
+
+except Exception as e:
+    print(f"Erro na sincronização: {e}")
