1d2a09abcdba6d2c4bb01381c50d1c35c711c0c2
Bruno Biazon <brunobiazon10@gmail.com>
2026-07-17 19:09:29 -0300

    Add token handling and resume functionality

--- garmin-extrator/sync_treino.py
+++ garmin-extrator/sync_treino.py
@@ -1,14 +1,32 @@
 import os
 import requests
+import base64
+import zipfile
+import garth
 from garminconnect import Garmin
 
 email = os.environ.get("GARMIN_EMAIL")
 senha = os.environ.get("GARMIN_PASSWORD")
 webhook_n8n = os.environ.get("N8N_WEBHOOK")
+token_b64 = os.environ.get("GARMIN_TOKEN")
 
 try:
     client = Garmin(email, senha)
-    client.login()
+
+    if token_b64:
+        
+        with open("tokens.zip", "wb") as f:
+            f.write(base64.b64decode(token_b64))
+            
+        with zipfile.ZipFile("tokens.zip", 'r') as zip_ref:
+            zip_ref.extractall("garmin_tokens")
+            
+        garth.resume("garmin_tokens")
+        client.garth = garth.client
+        
+    else:
+        client.login()
+        
     treinos = client.get_activities(0, 5) 
 
     if treinos:
@@ -28,7 +46,6 @@ try:
             })
             
         resposta = requests.post(webhook_n8n, json={"treinos": payload})
-        print(f"Webhook acionado. Status: {resposta.status_code}")
 
 except Exception as e:
     print(f"Erro na sincronização: {e}")
